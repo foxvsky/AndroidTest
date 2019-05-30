27e95d43672483823c5840f32a113d55a1a75771
Tien Ho <tien.hovt@zaisan.net>
2019-05-30 13:53:49 +0700

    fix

--- app/src/main/java/com/example/tienho_firstapp/androidtestapplication/MainActivity.kt
+++ app/src/main/java/com/example/tienho_firstapp/androidtestapplication/MainActivity.kt
@@ -17,8 +17,10 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
+
+        var abc: Int = 1
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
